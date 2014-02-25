2233c37fd50764494a910148ec81be63e24dd770
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-02-25 22:56:33 +0700

    Add equal

--- src/itb/ai/tubes1/entity/Barang.java
+++ src/itb/ai/tubes1/entity/Barang.java
@@ -57,4 +57,36 @@ public class Barang {
     public void setRestockPerHari(int _restockPerHari) {
 	restockPerHari = _restockPerHari;
     }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + kode;
+	return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj) {
+	    return true;
+	}
+	if (obj == null) {
+	    return false;
+	}
+	if (getClass() != obj.getClass()) {
+	    return false;
+	}
+	Barang other = (Barang) obj;
+	if (kode != other.kode) {
+	    return false;
+	}
+	return true;
+    }
 }
