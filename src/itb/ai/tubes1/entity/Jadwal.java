8eda0868a0628c08a6f24189ffe483541c67e5c4
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-02-25 22:48:19 +0700

    Update jadwal

--- src/itb/ai/tubes1/entity/Jadwal.java
+++ src/itb/ai/tubes1/entity/Jadwal.java
@@ -2,11 +2,36 @@ package itb.ai.tubes1.entity;
 
 import java.util.ArrayList;
 
+/**
+ * Class yang menyimpan data jadwal, bisa jadwal cewek atau tempat
+ */
 public class Jadwal {
+    
+    /**
+     * Jumlah minggu, di testcase 2
+     */
+    public static int JUMLAH_MINGGU;
+    
+    /**
+     * Jumlah jam, default 2
+     */
+    public static int JUMLAH_JAM = 2;
+    
+    /**
+     * Data array yang menyimpan jadwal
+     */
+    private ArrayList<Boolean> data;
+    
+    public Jadwal() {
+	data = new ArrayList<Boolean>();
+    }
 
     public ArrayList<Boolean> getList() {
-	// TODO Auto-generated method stub
-	return null;
+	return data;
+    }
+    
+    public void add(Boolean available) {
+	data.add(available);
     }
 
 }
