9fd09da01ed2283dc80575778c8782d76a75b6c5
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-03-03 19:24:33 +0700

    Add mutate

--- src/itb/ai/tubes1/controller/Solution.java
+++ src/itb/ai/tubes1/controller/Solution.java
@@ -1,8 +1,7 @@
 package itb.ai.tubes1.controller;
 
 import java.util.List;
-
-import Demo.MyVector;
+import java.util.Random;
 
 import com.lagodiuk.ga.Chromosome;
 
@@ -11,38 +10,43 @@ import com.lagodiuk.ga.Chromosome;
  * Solusi merepresentasikan jadwal
  */
 public class Solution implements Chromosome<Solution> {
+    	
+    private static final Random random = new Random();
 
-	public String data = new String();
+    public String data = new String();
 
-	@Override
-	protected Solution clone() {
-	    Solution clone = new Solution();
-	    clone.data = this.data;
-	    return clone;
-	}
+    @Override
+    protected Solution clone() {
+	Solution clone = new Solution();
+	clone.data = this.data;
+	return clone;
+    }
 	
-	/**
-	 * Returns clone of current chromosome, which is mutated a bit
-	 */
-	@Override
-	public Solution mutate() {
-	    Solution result = this;
+    /**
+     * Returns clone of current chromosome, which is mutated a bit
+     */
+    @Override
+    public Solution mutate() {
+	Solution result = this.clone();
 	    
-	    //result.data
+	int index = random.nextInt(result.data.length());
+	char[] data = result.data.toCharArray();
+	data[index] = '0';
+	result.data = new String(data);
 	    
-	    return result;
-	}
+	return result;
+    }
 
-	/**
-	 * Returns list of siblings <br/>
-	 * Siblings are actually new chromosomes, <br/>
-	 * created using any of crossover strategy
-	 */
-	@Override
-	public List<Solution> crossover(Solution anotherChromosome) {
+    /**
+     * Returns list of siblings <br/>
+     * Siblings are actually new chromosomes, <br/>
+     * created using any of crossover strategy
+     */
+    @Override
+    public List<Solution> crossover(Solution anotherChromosome) {
 	
-		// TODO Auto-generated method stub
-		return null;
-	}
+	// TODO Auto-generated method stub
+	return null;
+    }
 
 }
